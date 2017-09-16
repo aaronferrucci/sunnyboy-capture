97ed40662ea19d90853833753b0650d4cd005884
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2017-09-16 13:02:36 -0700

    Checkpoint: class-based wrapper around request-promise.

--- main2.js
+++ main2.js
@@ -3,10 +3,29 @@ var password = require('./password.js');
 var server = "https://192.168.1.140";
 var SunnyCapture = require('./lib/classTest.js');
 
-function main(params) {
-  var sc = new SunnyCapture(server, params.date, params.password);
+var dataHandler = function(resp) {
+  console.log("dataHandler()");
+  console.log(resp.headers.join(", "));
+  var i, len;
+  for (len = resp.data.length, i = 0; i < len; i++) {
+    console.log([
+      resp.data[i].date,
+      resp.data[i].time,
+      resp.data[i].power,
+      resp.data[i].raw
+    ].join(", "));
+  }
+}
 
-  return sc.login()
+function main(params) {
+  var sc = new SunnyCapture({
+    server: server,
+    date: params.date,
+    password: params.password,
+    handler: dataHandler,
+  });
+
+  var result = sc.login()
     .then(sc.logger.bind(sc))
     .finally(sc.logout.bind(sc));
 }
