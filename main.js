f81cc8131c18a4d91e85db95b86ed4fdeb25c9f9
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2017-09-16 20:13:01 -0700

    Capture firmware version, serial number, ip address.

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ var SunnyCapture = require('./lib/sunnyboy-capture.js');
 var password = require('./password.js');
 var server = "https://192.168.1.140";
 
+const util = require('util');
+var printKeyValue = function(prefix, obj) {
+  console.log(prefix + util.inspect(obj, {showHidden: false, depth: null}))
+}
+
 var dataHandler = function(resp) {
   console.log(resp.headers.join(", "));
   var i, len;
@@ -15,15 +20,21 @@ var dataHandler = function(resp) {
   }
 }
 
+var infoHandler = function(resp) {
+  printKeyValue("info: ", resp);
+}
+
 function main(params) {
   var sc = new SunnyCapture({
     server: server,
     date: params.date,
     password: params.password,
-    handler: dataHandler,
+    dataHandler: dataHandler,
+    infoHandler: infoHandler,
   });
 
   var result = sc.login()
+    .then(sc.devinfo)
     .then(sc.logger)
     .finally(sc.logout);
 }
